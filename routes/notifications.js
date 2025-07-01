4e992ec146f8a51ac873bed82c78478814a775fb
Quốc Quân <116168750+qquan2108@users.noreply.github.com>
2025-07-01 10:11:17 +0700

    Add FAQ admin page

--- routes/notifications.js
+++ routes/notifications.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router  = express.Router();
 const ctrl    = require('../controllers/notificationCtrl');
+const stream  = require('../utils/notificationStream');
 
 router.post('/',     ctrl.create);
 router.get('/',      ctrl.list);
 router.patch('/:id/read', ctrl.markRead);
 router.delete('/:id',     ctrl.remove);
+router.get('/stream', (req, res) => stream.addClient(res));
 
 module.exports = router;
