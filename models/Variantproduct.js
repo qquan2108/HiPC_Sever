d18d3365b8d3d749a68ca6557795716d10f772cc
Quốc Quân <116168750+qquan2108@users.noreply.github.com>
2025-08-27 01:22:41 +0700

    feat: enable soft delete for product variants

--- models/Variantproduct.js
+++ models/Variantproduct.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const softDelete = require('./plugins/softDelete');
+
 const VariantproductSchema = new mongoose.Schema({
   product_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
   name:           { type: String, required: true, trim: true },
@@ -6,5 +8,7 @@ const VariantproductSchema = new mongoose.Schema({
   stock:          { type: Number, default: 0, min: 0 }
   
 }, { timestamps: true });
+
+VariantproductSchema.plugin(softDelete);
 
 module.exports = mongoose.model('VariantProduct', VariantproductSchema);
