8c576f88ce9b57dca2acc5ea2d2f8e37ff08692f
Quốc Quân <116168750+qquan2108@users.noreply.github.com>
2025-08-27 00:31:48 +0700

    fix: include soft-deleted categories and brands in product edit

--- models/Category.js
+++ models/Category.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
+const softDelete = require('./plugins/softDelete');
 
 const categorySchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: String
+
 });
+
+categorySchema.plugin(softDelete);
 
 module.exports = mongoose.model('Category', categorySchema);
